1cb5b3dde285d5546778291c05e957c0f388f70d
elifcicekpamuk <cicekelif4250@gmail.com>
2026-06-30 14:17:31 +0300

    Create crypto.c

---
+++ crypto.c
@@ -0,0 +1,27 @@
+#include "crypto.h"
+#include <stdlib.h>
+
+#define VAULT_FILE "data/vault.gpg"
+#define TEMP_FILE "data/vault.txt"
+
+void decrypt_vault() {
+  char cmd[256];
+  snprintf(cmd, sizeof(cmd),  "gpg -d %s > %s", VAULT_FILE, TEMP_FILE);
+  system(cmd);
+}
+
+void encrypt_vault() {
+  char cmd[256];
+  snprintf(cmd, sizeof(cmd), "gpg -c --yes data/vault.txt");
+  system(cmd);
+}
+
+int vault_exists() {
+  FILE* f = fopen(VAULT_FILE, "r");
+  if(f==NULL) {
+    return 0;
+  }
+  fclose(f);
+  return 1;
+}
+
